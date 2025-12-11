05184be80d6e2cd57e916ea8602036756ed52ada
Jojorandet <josephine@randet.com>
2025-12-11 16:56:08 +0100

    switch case added, perror added and took away the unnecessary includes

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -1,10 +1,10 @@
 #include "ScalarConverter.hpp"
 
+static void	setType(const char* input, t_result *r);
 static void convertValues(t_result *r, char c);
 static void convertValues(t_result *r, int i);
 static void convertValues(t_result *r, double d);
 static void convertValues(t_result *r, float f);
-static void	setType(const char* input, t_result *r);
 
 ScalarConverter::ScalarConverter() {}
 
@@ -21,66 +21,93 @@ t_result	ScalarConverter::convert(const char* input)
 {
 	t_result		r;
 	setType(input, &r);
-	//char *endptr;
-	if (r.type == CHAR) { convertValues(&r, input[0]); }
-	else if (r.type == INT) { convertValues(&r, atoi(input)); }
-	else if (r.type == FLOAT) { convertValues(&r, std::strtof(input, NULL)); }
-	else if (r.type == DOUBLE) { convertValues(&r, strtod(input, NULL)); }
-	else if (r.type == INVALID) {
-		std::cout << "Error! Invalid literal type, cannot convert. Correct types needed:\n" \
-			"- char (./scalarconvert a)\n- int (decimal base)\n- double (4.2 for ex)\n" \
-			"- float (with '.' and f at end)" << std::endl;
-		
+	switch(r.type)
+	{
+		case (CHAR):
+			convertValues(&r, input[0]);
+			break;
+		case (INT):
+			convertValues(&r, atoi(input));
+			break;
+		case (FLOAT):
+			convertValues(&r, std::strtof(input, NULL));
+			break;
+		case (DOUBLE):
+			convertValues(&r, strtod(input, NULL));
+			break;
+		case(INVALID):
+			std::cout << RED << "\nError! Invalid literal type, cannot convert. Correct types needed:\n" \
+				"- char (./scalarconvert a)\n- int (decimal base)\n- double (4.2 for ex)\n" \
+				"- float (with '.' and f at end)" << RESET << std::endl;
+			break;
+		default:
+			break;
 	}
 	return r;
 }
 
-static void	setType(const char* input, t_result *r) {
-
+static void	setType(const char* input, t_result *r) 
+{
+	// initialisation
 	size_t	len = std::strlen(input);
 	r->type = INVALID;
-	if (len == 0)
-		return ;
-
+	if (len == 0) { return ; }
+	// check for the char
 	if (len == 1 && isalpha(input[0])) {
-		std::cout << "entered the function here" << '\n';
 		r->type = CHAR;
 		return ;
 	}
 
+	// preparation for double, float, and int for strtod, strtof, and strtol
+	errno = 0;
 	char	*end = NULL;
-	double result = std::strtod(input, &end);
-	if (*end == '\0' && (std::isinf(result) || std::isnan(result)))
-	{
-		r->type = DOUBLE;
-		return;
-	}
 
+	// float check bfore double 
 	if ( input[len - 1] == 'f')
 	{
 		std::strtof(input, &end);
+		if (errno == ERANGE) { perror("strtof"); r->type = INVALID; return; }
 		if (*end == 'f' && *(end + 1) == '\0')
 			r->type = FLOAT;
 		return ;
 	}
+	
+	// check for the isinf and isnan, 
+	double d_result = std::strtod(input, &end);
+	if (*end == '\0' && (std::isinf(d_result) || std::isnan(d_result))) { r->type = DOUBLE; return; }
 
+	// check for double
 	if (std::strchr(input, '.') != NULL)
 	{
 		std::strtod(input, &end);
-		if (*end == '\0' ) // Good this means it consumed the entire string
+		if (errno == ERANGE) { std::strerror(errno); r->type = INVALID; return; }
+		if (*end == '\0' )
 			r->type = DOUBLE;
 		return ;
 	}
 
-	std::strtol(input, &end, 10);
-	if (*end == '\0') // Good this means it consumed the entire string
+	std::cout << errno << '\n';
+	long n = std::strtol(input, &end, 10);
+	if (errno == EINVAL) { perror("strtol"); r->type = INVALID; return ; }
+	if (errno == ERANGE && n <= std::numeric_limits<int>::min())
+	{ 
+		perror("strtol");
+		r->type = INVALID;
+		return; 
+	}
+	if (errno == ERANGE && n >= std::numeric_limits<int>::max()) 
+	{ 
+		perror("strtol");
+		r->type = INVALID;
+		return;
+	}
+	if (*end == '\0')
 		r->type = INT;
 	return ;
 }
 
 static void convertValues(t_result *r, char c)
 {
-	//std::cout << &r->value << std::endl;
 	r->value.c = c;
 	r->value.i = static_cast<int>(c);
 	r->value.d = static_cast<double>(c);
@@ -89,7 +116,6 @@ static void convertValues(t_result *r, char c)
 
 static void convertValues(t_result *r, int i)
 {
-	std::cout << "entered the int convert values function" << '\n';
 	r->value.c = static_cast<char>(i);
 	r->value.i = i;
 	r->value.d = static_cast<double>(i);
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -4,12 +4,25 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <limits>
 #include <cstring>
 #include <cstdlib>
-#include <inttypes.h>
+#include <cstdio>
 #include <sstream>
 #include <cmath>
+#include <cerrno>
+#include <limits>
+
+#ifndef COLORS
+#define COLORS
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define BOLD    "\033[1m"
+#endif
 
 enum Type { CHAR, INT, DOUBLE, FLOAT, INVALID };
 
