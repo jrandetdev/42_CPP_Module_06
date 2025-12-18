8856651fa86e55efb3f7db8d97a2a2b53fd32098
Jojorandet <josephine@randet.com>
2025-12-18 16:53:15 +0100

    ready to push after 2 weeks hahahaha

--- ex01/Serializer.cpp
+++ ex01/Serializer.cpp
@@ -12,6 +12,16 @@ Serializer& Serializer::operator=(Serializer &other)
 
 Serializer::~Serializer() {}
 
+/**
+ * @brief Illustrates the usage of the reinterpret_cast and its use to convert
+ * integer types to pointer types and vice versa.
+ * 
+ * It garantees that if you cast a pointer to a different type and then reinterpret cast
+ * it back to its original type, we will get the original value. 
+ * 
+ * @param ptr 
+ * @return uintptr_t 
+ */
 uintptr_t	Serializer::serialize(Data *ptr)
 {
 	return (reinterpret_cast<uintptr_t>(ptr));
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,14 +5,23 @@
 
 int main()
 {
-	Data *test = NULL;
-	Data *deserialized_ptr = NULL;
-	std::cout << test << std::endl;
+	Data a;
+	Data *ptr = &a;
+	ptr->a = 2;
+	std::cout << "Data *ptr at address " << ptr << " with int a = " << ptr->a << std::endl;
 	
-	deserialized_ptr = Serializer::deserialize(Serializer::serialize(test));
-	if (deserialized_ptr == test)
-		std::cout << "Both pointers are equal after serialisation and deserialisation" << '\n';
+	std::cout << "\nSerialising ptr to convert from Data *ptr to integer type uintptr..." << '\n';
+	uintptr_t serialised_data = Serializer::serialize(ptr);
+	std::cout << "The uintptr_t serialised_data contains " << serialised_data << std::endl;
+	
+	std::cout << "\nDeserialising serialised_data back to Data *ptr type" << std::endl;
+	Data *ptr2 = Serializer::deserialize(serialised_data);
+	std::cout << "Data *ptr2 at address " << ptr << " with int a = " << ptr->a << std::endl;
+	
+	std::cout << "\nChecking that the deserialised uintptr converted back to Data * compares equal to the same address..." << std::endl;
+	if (ptr2 == ptr)
+		std::cout << "ptr and ptr2 have the same address and data" << std::endl;
 	else
-		std::cout << "Operation failed!" << std::endl;
-	return 0;
+		std::cout << "test failed" << '\n';
+	return (0);
 }
